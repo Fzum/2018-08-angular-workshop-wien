157205b6c09bb8db0b0afc40643ee97c792fc3a1
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-07-31 15:29:35 +0200

    SharedModule

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
+import { ButtonSharedModule } from './button-shared/button-shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BooksModule } from './books/books.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BooksModule
+    BooksModule,
+    ButtonSharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { ButtonSharedModule } from '../button-shared/button-shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    ButtonSharedModule
   ],
   declarations: [
     DashboardComponent,
