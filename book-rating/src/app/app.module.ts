f33fda326b2bcb61fc3650339071cf976e445c36
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-08-01 14:06:11 +0200

    Sprache einstellen: LOCALE_ID

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { ButtonSharedModule } from './button-shared/button-shared.module';
+import { registerLocaleData } from '@angular/common';
+import localeAt from '@angular/common/locales/de-AT'; 
 
 @NgModule({
   declarations: [
@@ -16,7 +18,13 @@ import { ButtonSharedModule } from './button-shared/button-shared.module';
     BooksModule,
     ButtonSharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de-AT' }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    registerLocaleData(localeAt);
+  }
+}
