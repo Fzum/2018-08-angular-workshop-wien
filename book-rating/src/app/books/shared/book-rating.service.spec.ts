b1f724aa06bba4880d094cc7aadcddd2784e1ff0
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-08-01 11:23:50 +0200

    Tests für RatingService

--- book-rating/src/app/books/shared/book-rating.service.spec.ts
+++ book-rating/src/app/books/shared/book-rating.service.spec.ts
@@ -1,15 +1,59 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { BookRatingService } from './book-rating.service';
+import { Book } from './book';
 
 describe('BookRatingService', () => {
+  let book: Book;
+  let rs: BookRatingService;
+
+  beforeEach(() => {
+    book = {
+      isbn: '',
+      title: '',
+      description: '',
+      rating: 3
+    }
+  });
+  
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [BookRatingService]
     });
+
+    rs = TestBed.get(BookRatingService);
   });
 
   it('should be created', inject([BookRatingService], (service: BookRatingService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should rate up a book by 1', () => {
+    const ratedBook = rs.rateUp(book);
+    expect(ratedBook.rating).toBe(4);
+    // expect(ratedBook.rating).toBe(book.rating + 1);
+  });
+
+  it('should rate down a book by 1', () => {
+    const ratedBook = rs.rateDown(book);
+    expect(ratedBook.rating).toBe(2);
+  });
+
+  it('should not be allowed to rate higher than 5', () => {
+    book.rating = 5;
+    const ratedBook = rs.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+  
+  it('should not be allowed to rate lower than 1', () => {
+    book.rating = 1;
+    const ratedBook = rs.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
+  });
+
+  it('should return a new book', () => {
+    const ratedBook = rs.rateUp(book);
+    expect(ratedBook).not.toBe(book);
+    expect(book.rating).toBe(3);
+  });
 });
--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -9,10 +9,10 @@ export class BookRatingService {
   constructor() { }
 
   rateUp(book: Book): Book {
-    return { ...book, rating: book.rating + 1 };
+    return { ...book, rating: book.rating < 5 ? book.rating + 1 : book.rating };
   }
 
   rateDown(book: Book): Book {
-    return { ...book, rating: book.rating - 1 };
+    return { ...book, rating: Math.max(1, book.rating - 1) };
   }
 }
