1d172228fc160aa21f7950165f472b106d52ed39
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-08-02 12:16:42 +0200

    Validierung, create-Event

--- book-rating/src/app/books/create-book/create-book.component.ts
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-create-book',
@@ -9,15 +10,38 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class CreateBookComponent implements OnInit {
 
   bookForm: FormGroup;
+  @Output() create = new EventEmitter<Book>();
 
   constructor() { }
 
   ngOnInit() {
     this.bookForm = new FormGroup({
-      isbn: new FormControl(''),
-      title: new FormControl(''),
+      isbn: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13)
+      ]),
+      title: new FormControl('', [
+        Validators.required
+      ]),
       description: new FormControl('')
     });
+  }
+
+  logForm() {
+    console.log(this.bookForm);
+  }
+
+  submitForm() {
+    const value = this.bookForm.value;
+    const newBook: Book = {
+      isbn: value.isbn,
+      title: value.title,
+      description: value.description,
+      rating: 1
+    }
+
+    this.create.emit(newBook);
   }
 
 }
