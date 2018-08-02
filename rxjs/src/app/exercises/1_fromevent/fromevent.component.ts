66d10710d5f68ca3b262f352c44ebf28e9e08ade
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-08-02 16:33:33 +0200

    RxJS Exercises 1/2

--- rxjs/src/app/exercises/1_fromevent/fromevent.component.ts
+++ rxjs/src/app/exercises/1_fromevent/fromevent.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { map, startWith, debounceTime } from 'rxjs/operators';
+import { MapOperator } from '../../../../node_modules/rxjs/internal/operators/map';
 
 @Component({
   selector: 'rxw-fromevent',
@@ -12,6 +13,11 @@ export class FromeventComponent implements OnInit {
   currentWidth: number = 0;
 
   ngOnInit() {
+    fromEvent(window, 'resize').pipe(
+      debounceTime(300),
+      map((e: any) => e.currentTarget.innerWidth),
+      startWith(window.innerWidth),
+    ).subscribe(width => this.currentWidth = width);
   }
 
 }
